77fdfcf805ff4a44b5610e4fb26df96a7cff00ac
d3dx13 <zhidkov2846@gmail.com>
2023-05-09 12:12:06 +0300

    log.txt

--- LaundryUpdaterThread.py
+++ LaundryUpdaterThread.py
@@ -66,9 +66,7 @@ async def skip_exceptions(awaitable):
 async def create_log(number, user_id):
     chat = await ext_bot.get_chat(chat_id=user_id)
     log_text = f"{datetime.now()} | @{chat.username} | {number}\n"
-    f = open("log.txt", "a+")
-    f.write(log_text)
-    f.close()
+    print(log_text)
 
 
 def wait(user_id, numbers):
